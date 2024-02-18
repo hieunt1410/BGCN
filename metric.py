d4103fa058733be67c936b93614ed74a51e3bc55
UwU-tao <hieungo1410@gmail.com>
2024-02-18 18:42:22 +0700

    add

--- metric.py
+++ metric.py
@@ -84,20 +84,20 @@ class Jaccard(_Metric):
     Jaccard in top-k samples
     '''
 
-    def __init__(self):
+    def __init__(self, topk):
         super().__init__()
         self.epison = 1e-8
+        self.topk = topk
         
     def get_title(self):
-        return "Jaccard"
+        return "Jaccard@{}".format(self.topk)
 
     def __call__(self, scores, ground_truth):
-        is_hit = get_is_hit(scores, ground_truth, scores.shape[1])
+        is_hit = get_is_hit(scores, ground_truth, self.topk)
         is_hit = is_hit.sum(dim=1)
-        gold_bun = ground_truth.sum(dim=1)
-        pred_bun = scores.sum(dim=1)
-        self._cnt += scores.shape[0] - (gold_bun == 0).sum().item()
-        self._sum += (is_hit/(gold_bun+pred_bun-is_hit)).sum().item()
+        num_pos = ground_truth.sum(dim=1)
+        self._cnt += scores.shape[0] - (num_pos == 0).sum().item()
+        self._sum += (is_hit/(2 * self.topk-is_hit)).sum().item()
 
 class NDCG(_Metric):
     '''
